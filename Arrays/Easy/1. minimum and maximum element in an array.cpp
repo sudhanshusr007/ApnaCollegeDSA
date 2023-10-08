f7497390c1f6332b539ce91a2447136cdc50c327
Sudhanshu Ranjan <sudhanshusr007@gmail.com>
2023-10-08 18:55:33 +0530

    new file added

--- Arrays/Easy/1. minimum and maximum element in an array.cpp
+++ Arrays/Easy/1. minimum and maximum element in an array.cpp
@@ -1,18 +1,39 @@
-pair<long long, long long> getMinMax(long long a[], int n) {
-    if (n == 0) {
-        return {LLONG_MAX, LLONG_MIN};
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
     }
+    return 0;
+}
+// } Driver Code Ends
 
-    long long minVal = a[0];
-    long long maxVal = a[0];
 
-    for (int i = 1; i < n; ++i) {
-        if (a[i] < minVal) {
-            minVal = a[i];
-        } else if (a[i] > maxVal) {
-            maxVal = a[i];
+pair<long long, long long> getMinMax(long long a[], int n) {
+    int min = a[0];
+    int max = a[0];
+    
+    for(int i = 0;i<n;i++){
+        if(a[i]<min){
+            min = a[i];
+        }
+        else if(a[i]>max){
+            max = a[i];
         }
     }
-
-    return {minVal, maxVal};
+    return {min,max};
 }
